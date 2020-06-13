916ffce3422d2d829b47e0d168d5695262cfa4e8
Dan Finlay <dan@danfinlay.com>
2020-06-12 18:31:06 -0700

    Use deno testing conventions

--- src/random/test.ts
+++ src/random/test.ts
@@ -1,13 +1,20 @@
 import randomFromSeed from './randomFromSeed.ts';
+import { assert } from "https://deno.land/std/testing/asserts.ts";
 
-const input1 = 'fffffffff';
-const output1 = randomFromSeed(input1);
-console.log(`Input ${input1} produces ${output1}`);
+Deno.test('high random input', () => {
+  const input1 = 'fffffffff';
+  const output1 = randomFromSeed(input1);
+  assert(output1 > 0.99, 'outputs a high value');
+})
 
-const input2 = '000000000';
-const output2 = randomFromSeed(input2);
-console.log(`Input ${input2} produces ${output2}`);
+Deno.test('zero random input', () => {
+  const input2 = '000000000';
+  const output2 = randomFromSeed(input2);
+  assert(output2 === 0, 'hex zero input equals zero');
+})
 
-const input3 = '800000';
-const output3 = randomFromSeed(input3);
-console.log(`Input ${input3} produces ${output3}`);
+Deno.test('half random input', () => {
+  const input3 = '800000';
+  const output3 = randomFromSeed(input3);
+  assert(output3 === 0.5, 'hex 8000 is one half');
+})
